fb309822c00bc752c86cc7887d74cd0723f9dd2d
Arina Kharchenko <arina28782005@gmail.com>
2023-03-30 09:26:19 +0200

    added the eighth

---
+++ idea/eighth.cpp
@@ -0,0 +1,37 @@
+#include <QMessageBox>
+#include <QPixmap>
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPixmap pix("C:/Users/User/Desktop/Presentations/qt_pic/login-icon.png");
+    int w = ui->label_pic->width();
+    int h = ui->label_pic->height();
+    ui->label_pic->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
+    ui->statusbar->addPermanentWidget(ui->label_3);
+    ui->statusbar->addPermanentWidget(ui->progressBar);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_Login_clicked()
+{
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
+
+    if (username == "test" && password == "test") 
+    {
+        ui->statusbar->showMessage("Username and password is correct", 5000);
+        ui->label_3->setText("Username and password is correct");
+    }
+    else 
+    {
+        ui->statusbar->showMessage("Username and password is not correct", 5000);
+        ui->label_3->setText("Username and password is not correct");
+    }
+}
+
